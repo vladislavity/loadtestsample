655aa1294454fd0224fcf257026a0ef6adaecb1d
vsvietlichnyi <vladyslav.svietlichnyi@leongaming.com>
2021-02-21 12:49:18 +0200

    optimized imports

--- src/main/java/test/GetGoogleTest.java
+++ src/main/java/test/GetGoogleTest.java
@@ -4,13 +4,8 @@ import org.jsmart.zerocode.core.domain.TargetEnv;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
